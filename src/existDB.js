9ed466df01cd71ac79fde2365ac2db001a12452d
root <root@copyrightcentral.arts.gla.ac.uk>
2017-04-18 15:21:09 +0100

    new queries

--- src/existDB.js
+++ src/existDB.js
@@ -11,13 +11,7 @@ var db = new exist.DB('http://localhost:8080', {
 
 export async function textSearch(query){
 
-    var query = 'xquery version "3.1";'
-                  +'declare default element namespace "http://www.tei-c.org/ns/1.0";'
-                  +'declare namespace tei="http://www.tei-c.org/ns/1.0";'
-                  +'<entries>'
-                  +'{'
-                  +'for $hit in collection("/db/SRO")//tei:div[ft:query(., "'+query+'")]'
-                  +'let $score as xs:float := ft:score($hit) where $hit/@type="entry" order by $score descending return <entry> <docid>{data($hit/@xml:id)}</docid> <score>{data($score)}</score> <date>{data($hit//date)}</date> <stationers>{ for $stat in $hit//persName where $stat/@role="stationer" return <name> {string-join( ($stat/forename,$stat/surname), ", ")} </name> } </stationers> <doc>{ for $item in $hit/p return <item>{fn:string-join($item//text()," ")}</item> }</doc> </entry> } </entries>'
+    var query = 'xquery version "3.1"; declare default element namespace "http://www.tei-c.org/ns/1.0"; declare namespace tei="http://www.tei-c.org/ns/1.0"; declare namespace array="http://www.w3.org/2005/xpath-functions/array"; let $pageLimit as xs:decimal := 20 let $page as xs:decimal := 30 let $allResults := array { for $coll in collection("/db/SRO/docs")//tei:div[ft:query(., "'+query+'")] let $hits := $coll//tei:div[@type="entry"] return $hits} let $resultsCount as xs:decimal := array:size($allResults) let $maxpage as xs:double := math-ext:ceil($resultsCount div $pageLimit) let $firstEntry := if ( $page > $maxpage ) then ($maxpage * $pageLimit) - ($pageLimit - 1) else ($page * $pageLimit) - ($pageLimit - 1) let $offset := if ( ($firstEntry + $pageLimit) > $resultsCount ) then ($firstEntry + $pageLimit) - $resultsCount else 0 let $pagesToReturn := $pageLimit - $offset return <results> <paging> <current>{$page}</current> <last>{$maxpage}</last> <returned>{$pagesToReturn}</returned> <total>{$resultsCount}</total> </paging> <entries> { for $hit in array:flatten(array:subarray($allResults, $firstEntry, $pagesToReturn)) return <entry> <docid>{data($hit/@xml:id)}</docid> <date>{data($hit//date/@notBefore)}</date> <people>{ for $pers in $hit//persName return <person> <role>{data($pers/@role)}</role> <name> <title> {normalize-space($pers/text()[last()])} </title> <forename>{$pers/forename/text()}</forename> <surname>{$pers/surname/text()}</surname> </name> </person> } </people> <items>{ for $item in $hit/p return <item> { $item } </item> }</items> </entry> } </entries> </results>'
     //console.log(query);
     return new Promise( function (Resolve,Reject){
 
